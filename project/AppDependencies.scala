3195c329eccc377d11d8c87836aee7111005bb81
DerekPalmer <35488891+derekspalmer@users.noreply.github.com>
2021-09-08 18:24:34 +0100

    MMCA-3055: remove play-frontend-govuk dependency (#4)
    * MMCA-3055: remove play-frontend-govuk dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,11 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.4.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "0.76.0-play-28",
-    "uk.gov.hmrc" %% "play-frontend-govuk" % "0.77.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.4.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
     "com.typesafe.play" %% "play-json-joda" % "2.9.2",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.50.0",
     "org.webjars.npm" % "moment" % "2.29.1",
-    "org.webjars.npm" % "hmrc-frontend" % "1.5.0",
     "uk.gov.hmrc" %% "tax-year" % "1.3.0"
   )
 
