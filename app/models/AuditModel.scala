8ac3ca0b06d0cb5f4eea2584fa652682bb19821d
bsgangapat <106598990+bsgangapat@users.noreply.github.com>
2022-12-13 13:09:41 +0000

    MMCA-3700 Amend field guaranteeAccountNumber on audit event

--- app/models/AuditModel.scala
+++ app/models/AuditModel.scala
@@ -23,7 +23,7 @@ import java.time.LocalDate
 
 case class AuditModel(auditType: String, transactionName: String, detail: JsValue)
 
-case class GuaranteeCsvAuditData(eori: EORI, gan: String, openOrClosed: String, asOfDateTime: String, fileFormat: String, from: Option[LocalDate], to: Option[LocalDate])
+case class GuaranteeCsvAuditData(eori: EORI, guaranteeAccountNumber: String, openOrClosed: String, asOfDateTime: String, fileFormat: String, from: Option[LocalDate], to: Option[LocalDate])
 
 object GuaranteeCsvAuditData {
   implicit val guaranteeCsvAuditDataWrites: Writes[GuaranteeCsvAuditData] = Json.writes[GuaranteeCsvAuditData]
