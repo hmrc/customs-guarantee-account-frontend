1e7ffcb4cedc0017bf9d77c49a82cf01216e02f9
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-07-07 16:27:53 +0100

    Fixing the Date error identification (#54)

---
+++ app/helpers/FormHelper.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helpers
+
+object FormHelper {
+
+  /**
+   * Updates the key as per below validation
+   * If key is either start or end and error msg is among future date,etmp date or tax year date
+   *  Updated key - start.month
+   *
+   *  If key is either start or end and error msg is of invalid year length
+   *   Updated key - start.year
+   *
+   *  If key is other than start or end
+   *   Updated key is unchanged
+   *
+   * @return Updated FormError key
+   */
+  def updateFormErrorKeyForStartAndEndDate(): (String, String) => String = (key: String, errorMsg: String) => {
+    
+    val olderStartDateKey = "cf.form.error.start.date-too-far-in-past"
+    val earlierStartDateKey = "cf.form.error.startDate.date-earlier-than-system-start-date"
+    val futureStartDateKey = "cf.form.error.start-future-date"
+    val invalidStartMonthKey = "cf.form.error.start.month.date-number-invalid"
+    val invalidStartYearKey = "cf.form.error.start.year.date-number-invalid"
+
+    val olderEndDateKey = "cf.form.error.end.date-too-far-in-past"
+    val earlierEndDateKey = "cf.form.error.endDate.date-earlier-than-system-start-date"
+    val futureEndDateKey = "cf.form.error.end-future-date"
+    val invalidEndMonthKey = "cf.form.error.end.month.date-number-invalid"
+    val invalidEndYearKey = "cf.form.error.end.year.date-number-invalid"
+
+    
+    val startDateMsgKeyList = List(olderStartDateKey,earlierStartDateKey,futureStartDateKey,invalidStartMonthKey,invalidStartYearKey)
+    val endDateMsgKeyList = List(olderEndDateKey, earlierEndDateKey, futureEndDateKey, invalidEndMonthKey, invalidEndYearKey)
+
+    if ((key.equals("start") || key.equals("end"))) {
+      if (startDateMsgKeyList.contains(errorMsg) || endDateMsgKeyList.contains(errorMsg))
+        s"$key.month"
+      else
+        s"$key.year"
+    } else {
+      key
+    }
+  }
+}
---
+++ test/helpers/FormHelperSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helpers
+
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import utils.SpecBase
+
+class FormHelperSpec extends SpecBase {
+  "updateFormErrorKeyForStartAndEndDate" must {
+    "append .month in the FormError key when key is either start or end and error msg key is " +
+      "olderStartDateKey, earlierStartDateKey, futureStartDateKey, invalidStartMonthKey and invalidStartYearKey" in new SetUp {
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.start.date-too-far-in-past") shouldBe s"$startKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.startDate.date-earlier-than-system-start-date") shouldBe s"$startKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.start-future-date") shouldBe s"$startKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.start.month.date-number-invalid") shouldBe s"$startKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.start.year.date-number-invalid") shouldBe s"$startKey.month"
+
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.end.date-too-far-in-past") shouldBe s"$endKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.endDate.date-earlier-than-system-start-date") shouldBe s"$endKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.end-future-date") shouldBe s"$endKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.end.month.date-number-invalid") shouldBe s"$endKey.month"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.end.year.date-number-invalid") shouldBe s"$endKey.month"
+    }
+
+    "append .year in the FormError key when key is either start or end and " +
+      "error msg key is of invalid year length" in new SetUp {
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        startKey, "cf.form.error.year.invalid") shouldBe s"$startKey.year"
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        endKey, "cf.form.error.year.invalid") shouldBe s"$endKey.year"
+    }
+
+    "return the unchanged key when key in neither start or end" in new SetUp {
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        defaultKey, "cf.form.error.year.invalid") shouldBe defaultKey
+
+      FormHelper.updateFormErrorKeyForStartAndEndDate()(
+        defaultKey, "cf.form.error.year.invalid") shouldBe defaultKey
+    }
+  }
+  trait SetUp {
+    val startKey = "start"
+    val endKey = "end"
+    val defaultKey = "default"
+  }
+}
