10c0cb9e3b03275c2c4024bfdd900b6cd340962d
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-07-19 11:48:47 +0100

    [MMCA-4104] make val private and add type for test and compile vals

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.19.0"
+  private val bootstrapVersion = "7.19.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
@@ -15,7 +15,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
     "org.scalatest" %% "scalatest" % "3.2.9" % Test,
     "org.jsoup" % "jsoup" % "1.10.2" % Test,
