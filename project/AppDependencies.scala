16a21a924950a3d4758c6e2ea0ae5c3e48855ea6
tyagikumarvineet <tyagi.kumar.vineet@gmail.com>
2023-07-19 11:21:14 +0100

    [MMCA-4104] Update library version to the latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,20 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.15.0"
+
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.7.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
     "com.typesafe.play" %% "play-json-joda" % "2.9.2",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.2.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "org.webjars.npm" % "moment" % "2.29.1",
     "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.15.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
     "org.scalatest" %% "scalatest" % "3.2.9" % Test,
     "org.jsoup" % "jsoup" % "1.10.2" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
