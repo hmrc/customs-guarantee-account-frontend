879b55a96f8db809a831ec557eb3c18db21c0c5a
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-08-17 18:18:21 +0100

    [MMCA-4174] Upgrade version for Non-HMRC dependencies (#58)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,20 +8,20 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
-    "org.typelevel" %% "cats-core" % "2.3.0",
-    "com.typesafe.play" %% "play-json-joda" % "2.9.2",
+    "org.typelevel" %% "cats-core" % "2.9.0",
+    "com.typesafe.play" %% "play-json-joda" % "2.9.4",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
-    "org.webjars.npm" % "moment" % "2.29.1",
+    "org.webjars.npm" % "moment" % "2.29.4",
     "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
-    "org.scalatest" %% "scalatest" % "3.2.9" % Test,
-    "org.jsoup" % "jsoup" % "1.10.2" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.16" % Test,
+    "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.typesafe.play" %% "play-test" % current % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test
   )
 }
