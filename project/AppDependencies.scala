414927c137494f97b4fb4431dc608bd104c4ff0a
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-10-02 05:40:35 +0100

    MMCA-4261 : Update version & configuration for CDS Financials service - Sept - V5

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "7.19.0"
+  private val bootstrapVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28",
     "org.typelevel" %% "cats-core" % "2.9.0",
     "com.typesafe.play" %% "play-json-joda" % "2.9.4",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "org.webjars.npm" % "moment" % "2.29.4",
-    "uk.gov.hmrc" %% "tax-year" % "3.2.0"
+    "uk.gov.hmrc" %% "tax-year" % "3.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -22,6 +22,7 @@ object AppDependencies {
     "com.typesafe.play" %% "play-test" % current % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.21.0-play-28" % Test
   )
 }
