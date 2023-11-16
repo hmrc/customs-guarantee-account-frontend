7f225545b440d5a6e0f411a64627a0f21542bc23
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2023-11-16 15:49:10 +0000

    [MMCA-4370] Adding EmailActionSpec

---
+++ test/controllers/actions/EmailActionSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+
+import models.UnverifiedEmail
+import models.request.IdentifierRequest
+import org.mockito.ArgumentMatchersSugar.any
+import org.scalatest.matchers.must.Matchers.{convertToAnyMustWrapper}
+import play.api.inject
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.DataStoreService
+import uk.gov.hmrc.auth.core.retrieve.Email
+import uk.gov.hmrc.http.ServiceUnavailableException
+import utils.SpecBase
+
+import scala.concurrent.Future
+
+class EmailActionSpec extends SpecBase {
+
+  "EmailAction" should {
+    "Let requests with validated email through" in new Setup {
+      running (app) {
+        when(mockDataStoreService.getEmail(any)(any)).thenReturn(Future.successful(Right(Email("last.man@standing.co.uk"))))
+        val response = await(emailAction.filter(authenticatedRequest))
+        response mustBe None
+      }
+    }
+
+
+    "Let request through, when getEmail throws service unavailable exception" in new Setup {
+      running(app){
+        when(mockDataStoreService.getEmail(any)(any)).thenReturn(Future.failed(new ServiceUnavailableException("")))
+        val response = await(emailAction.filter(authenticatedRequest))
+        response mustBe None
+      }
+    }
+
+    "Redirect users with unvalidated emails" in new Setup {
+      running(app) {
+        when(mockDataStoreService.getEmail(any)(any)).thenReturn(Future.successful(Left(UnverifiedEmail)))
+        val response = await(emailAction.filter(authenticatedRequest))
+        response.get.header.status mustBe SEE_OTHER
+        response.get.header.headers(LOCATION) must include("/verify-your-email")
+      }
+    }
+  }
+
+  trait Setup {
+    val mockDataStoreService: DataStoreService = mock[DataStoreService]
+
+    val app = application.overrides(
+      inject.bind[DataStoreService].toInstance(mockDataStoreService)
+    ).build()
+
+    val emailAction = app.injector.instanceOf[EmailAction]
+
+    val authenticatedRequest = IdentifierRequest(FakeRequest("GET","/"), "1234")
+  }
+}
