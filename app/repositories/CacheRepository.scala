762dad8c814ed3755cb51b7b4cdc8b90a46e887a
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-07-29 16:00:39 +0100

    [MMCA-4846] Refactor

--- app/repositories/CacheRepository.scala
+++ app/repositories/CacheRepository.scala
@@ -79,6 +79,7 @@ case class GuaranteeAccountMongo(transactions: Seq[EncryptedGuaranteeTransaction
                                  lastUpdated: Instant = Instant.now())
 
 object GuaranteeAccountMongo {
+  
   implicit val javaTimeFormat: Format[Instant] = Format[Instant](
     Reads.path.nullable[String](__ \ "$date" \ "$numberLong").map(dateString => dateValueFromString(dateString)),
     Writes.at[String](__ \ "$date" \ "$numberLong").contramap(_.toEpochMilli.toString)
@@ -92,6 +93,7 @@ object GuaranteeAccountMongo {
       case _ => Instant.now()
     }
   }
+
 }
 
 trait CacheRepository {
