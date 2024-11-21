7cec23ac358978769c69c4db6d06f43a008cb8df
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-11-21 16:35:59 +0000

    MMCA-4785 | Update Scala version, http, plugins & dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,27 +1,25 @@
-import play.core.PlayVersion.current
 import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.5.0"
+  private val bootstrapVersion = "9.5.0"
+  private val mongoVersion = "2.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.0.0",
-    "org.typelevel" %% "cats-core" % "2.10.0",
-    "com.typesafe.play" %% "play-json-joda" % "2.9.4",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "1.8.0",
-    "org.webjars.npm" % "moment" % "2.29.4",
-    "uk.gov.hmrc" %% "tax-year" % "4.0.0"
-  )
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.6.0",
+    "org.typelevel" %% "cats-core" % "2.12.0",
+    "com.typesafe.play" %% "play-json-joda" % "2.10.6",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % mongoVersion,
+    "org.webjars.npm" % "moment" % "2.30.1",
+    "uk.gov.hmrc" %% "tax-year" % "5.0.0")
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.scalatest" %% "scalatest" % "3.2.16" % Test,
-    "org.jsoup" % "jsoup" % "1.16.1" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.18" % Test,
+    "org.jsoup" % "jsoup" % "1.18.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.29" % Test,
-  )
+    "org.scalatestplus" %% "mockito-4-11" % "3.2.18.0" % Test)
 }
