aa4297f9ab047def7314c04d8306d397c813da95
James Crook <48724197+JoniBach@users.noreply.github.com>
2025-05-01 11:15:24 +0100

    MMCA-5295: Add test to Update GOV survey link

--- test/config/AppConfigSpec.scala
+++ test/config/AppConfigSpec.scala
@@ -27,5 +27,10 @@ class AppConfigSpec extends SpecBase {
         appConfig.appName mustBe "customs-guarantee-account-frontend"
       }
     }
+    
+    "contain the correct GOV survay banner URL" in {
+      appConfig.helpMakeGovUkBetterUrl mustBe
+        "https://survey.take-part-in-research.service.gov.uk/jfe/form/SV_74GjifgnGv6GsMC?Source=BannerList_HMRC_CDS_MIDVA"
+    }
   }
 }
